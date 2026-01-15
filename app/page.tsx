41a69762ff22078f8441f1b9f31ec5d924d49f23
Francesca Vago <vago.francesca@gmail.com>
2026-01-15 23:30:25 +0100

    feat: navigation menu component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import AIChatButton from '@/components/AIChat/AIChatButton';
+import NavMenu from '@/components/NavMenu/NavMenu';
 
 export default function Chat() {
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      <NavMenu />
       <AIChatButton />
     </div>
   );
