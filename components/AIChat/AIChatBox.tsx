965719430feeb23cec54c63d6c80b9ee64239592
Francesca Vago <vago.francesca@gmail.com>
2026-01-16 12:07:00 +0100

    style: style chat box

--- components/AIChat/AIChatBox.tsx
+++ components/AIChat/AIChatBox.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
 import AIChatMessage from "./AIChatMessage";
-import { SendHorizontal } from "lucide-react";
+import { SendHorizontal, XCircle } from "lucide-react";
 import { PopoverContent } from "../ui/popover";
 
 interface AIChatBoxProps {
@@ -18,21 +18,37 @@ export default function AIChatBox({ open, onClose }: AIChatBoxProps) {
 	if (!open) return null;
 
 	return (
-		<PopoverContent>
-			{messages.map(message => (
-				<AIChatMessage key={message.id} message={message} />
-			))}
+		<PopoverContent
+			side="bottom"
+			className="w-80"
+		>
+			<div className="grid gap-4">
+				<div className="space-y-2 flex items-center justify-between p-4">
+					<h4 className="leading-none font-medium">AI Assistant</h4>
+					<Button onClick={onClose} variant="ghost" className="hover:bg-transparent cursor-pointer">
+						<XCircle size={24} />
+					</Button>
+				</div>
+			</div>
+			<div className="mt-3 h-full px-3 overflow-y-auto">
+				{
+					messages.map(message => (
+						<AIChatMessage key={message.id} message={message} />
+					))
+				}
+			</div>
 
 			<form
-				className="flex gap-2"
+				className="m-3 flex gap-1 justify-between items-end"
 				onSubmit={e => {
 					e.preventDefault();
 					sendMessage({ text: input });
 					setInput('');
-				}}
+				}
+				}
 			>
 				<Textarea
-					className="cn-textarea placeholder:text-muted-foreground flex flex-grow field-sizing-content min-h-16 w-full outline-none disabled:cursor-not-allowed disabled:opacity-50 cn-input-group-textarea flex-1 resize-none"
+					className="resize-none flex-grow min-h-[40px] focus-visible:ring-0"
 					value={input}
 					placeholder="Ask something..."
 					onChange={e => setInput(e.currentTarget.value)}
@@ -43,11 +59,11 @@ export default function AIChatBox({ open, onClose }: AIChatBoxProps) {
 					size="icon-lg"
 					variant="default"
 					aria-label="Send"
-					className="disabled:opacity-50"
+					className="w-10 cursor-pointer"
 				>
 					<SendHorizontal />
 				</Button>
-			</form>
-		</PopoverContent>
+			</form >
+		</PopoverContent >
 	);
 }
--- components/AIChat/AIChatButton.tsx
+++ components/AIChat/AIChatButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Bot } from "lucide-react";
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import { Button } from "../ui/button";
 import AIChatBox from "./AIChatBox";
 import { Popover, PopoverTrigger } from "../ui/popover";
@@ -10,13 +10,12 @@ export default function AIChatButton() {
 	const [chatBoxOpen, setChatBoxOpen] = useState(false);
 
 	return (
-		<Popover>
+		<Popover open={chatBoxOpen} onOpenChange={setChatBoxOpen}>
 			<PopoverTrigger asChild>
 				<Button
-					onClick={() => setChatBoxOpen(!chatBoxOpen)}
 					size="icon-lg"
 					variant="default"
-					className="fixed bottom-4 right-4 z-40 bg-gradient-to-r from-purple-600 to-blue-500 text-white rounded-full p-4 shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110"
+					className="fixed bottom-4 right-4 bg-gradient-to-r from-purple-600 to-blue-500 text-white rounded-full p-4 shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110 cursor-pointer"
 				>
 					<Bot className="animate-pulse" />
 				</Button>
